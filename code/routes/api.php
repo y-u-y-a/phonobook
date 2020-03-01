e794667b9c959fc5459dab9d1c83ce06f6deb4a6
y-u-y-a <yuya_yamada00@yahoo.co.jp>
2020-03-01 15:05:29 +0900

    ルーティングにname付加

--- code/routes/api.php
+++ code/routes/api.php
@@ -3,40 +3,27 @@
 use Illuminate\Http\Request;
 
 
-// 全ての本を返す
-Route::get("/books/all", "BooksController@index");
-// 借りている本を返す
-Route::get("/books/borrowed", "BooksController@getBooksBorrowed");
-// idから本を返す
-Route::get("/books/{id}", "BooksController@show");
-// 返却処理
-Route::get("/books/return/{id}", "BooksController@return")->name("return");
+Route::get("/books/all", "BooksController@index")->name("get.all_books");
+Route::get("/books/borrowed", "BooksController@getBooksBorrowed")->name("get.borrowed_books");
+Route::get("/books/{id}", "BooksController@show")->name("get.book");
+Route::get("/books/return/{id}", "BooksController@return")->name("return.book");
 // 貸出処理(ログインユーザー)
-Route::get("/books/lend/{id}", "BooksController@lend")->name("lend");
+Route::get("/books/lend/{id}", "BooksController@lend")->name("lend.book");
 // 貸出処理(顔認証)
-Route::get("/books/{book_isbn}/borrow/{user_id}", "BooksController@borrow")->name("borrow");
+Route::get("/books/{book_isbn}/borrow/{user_id}", "BooksController@borrow")->name("borrow.book");
 // VRでバーコード画像からISBNのテキストを返す
 // Route::post("/books/barcode", "BooksController@getISBN");
-// 登録処理
-Route::post("/books", "BooksController@store");
-// 更新処理
-Route::patch("/books", "BooksController@update");
-// 削除処理
-Route::delete("/books", "BooksController@destroy");
+Route::post("/books", "BooksController@store")->name("store.book");
+Route::patch("/books", "BooksController@update")->name("update.book");
+Route::delete("/books", "BooksController@destroy")->name("destroy.book");
 
 
-// 全てのユーザーを返す
-Route::get("/users/all", "UsersController@index");
-// ログインユーザーを返す
-Route::get("/users/logined", "UsersController@getLoginUser");
-// VRによる顔認証処理(or null)
-Route::post("/users/authFace", "UsersController@authWithFace");
-// 出勤・退勤処理
-Route::post("/users/state", "UsersController@switchState");
-
-// 新規登録処理
+Route::get("/users/all", "UsersController@index")->name("get.all_users");
+Route::get("/users/logined", "UsersController@getLoginUser")->name("get.login_user");
 Route::post("/auth/register", "Auth\RegisterController@register")->name("register");
-// ログイン処理
 Route::post("/login", "Auth\LoginController@login")->name("login");
-// ログアウト処理
 Route::post("/logout", "Auth\LoginController@logout")->name("logout");
+// VRによる顔認証処理(or null)
+Route::post("/users/authFace", "UsersController@authWithFace")->name("auth_face");
+// 出勤・退勤処理
+Route::post("/users/state", "UsersController@switchState")->name("switch.state");
